dfc8ef5c9ea253dd7bb73f56023ff284001e0895
Elias-Dewa <elias.dewa19@gmail.com>
2023-04-18 07:44:30 -0700

    Update 101-iterateThroughObject.js

--- 0x00-ES6_basic/101-iterateThroughObject.js
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -3,5 +3,6 @@ export default function iterateThroughObject(reportWithIterator) {
   for (const employee of reportWithIterator) {
     employees.push(employee);
   }
+
   return employees.join(' | ');
 }
