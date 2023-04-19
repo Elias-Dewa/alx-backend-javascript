99d8b909a0848673df930fe0779db9efc1ca40e6
Elias-Dewa <elias.dewa19@gmail.com>
2023-04-19 12:23:16 -0700

    Update 100-createIteratorObject.js

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,6 +1,6 @@
 export default function createIteratorObject(report) {
   let answer = [];
-  for (const value of Object.values(report.answer)) {
+  for (const value of Object.values(report.allEmployees)) {
     answer = [...answer, ...value];
   }
   return answer;
