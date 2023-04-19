c503941253321f5119514530e26edb86324d985c
Elias-Dewa <elias.dewa19@gmail.com>
2023-04-19 10:06:00 -0700

    Update 6-final-user.js

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -10,12 +10,12 @@ export default function handleProfileSignup(firstName, lastName, fileName) {
   return Promise.allSettled(users).then((values) => {
     const arrayResult = [];
     values.forEach((value) => {
-      if (value.status === "fulfilled") {
+      if (value.status === 'fulfilled') {
         arrayResult.push(value);
       } else {
         arrayResult.push({
           status: value.status,
-          value: `Error: ${value.message}`,
+          value: `Error: ${value.reason.message}`,
         });
       }
     });
