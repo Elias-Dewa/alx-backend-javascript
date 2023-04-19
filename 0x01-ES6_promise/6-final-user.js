d9526dce8e3694eca94321f33338a145bc4f02c5
Elias-Dewa <elias.dewa19@gmail.com>
2023-04-19 09:45:43 -0700

    ES6_Promises: some mandatory tasks

---
+++ 0x01-ES6_promise/6-final-user.js
@@ -0,0 +1,24 @@
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
+
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  const users = [
+    signUpUser(firstName, lastName),
+    uploadPhoto(fileName),
+  ];
+
+  return Promise.allSettled(users).then((values) => {
+    const arrayResult = [];
+    values.forEach((value) => {
+      if (value.status === 'success') {
+        arrayResult.push(value);
+      } else {
+        arrayResult.push({
+          status: value.status,
+          value: `Error: ${value.message}`,
+        });
+      }
+    });
+    return arrayResult;
+  });
+}
