413af0d567ae20167124c84a4e4efe19193952a3
Elias-Dewa <elias.dewa19@gmail.com>
2023-04-25 12:13:33 -0700

    Update 4-update_grade_by_city.js

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,5 +1,12 @@
 export default function updateStudentGradeByCity(arrayOfStudents, city, newGrades) {
   return arrayOfStudents.filter((obj) => obj.location === city).map((obj) => {
-    const [newGrades] = newGrades.filter()
+    const newObj = { ...obj };
+    const newStudentObj = newGrades.find((student) => student.studentId === obj.id);
+    if (newStudentObj) {
+      newObj.grade = newStudentObj.grade;
+    } else {
+      newObj.grade = 'N/A';
+    }
+    return newObj;
   });
 }
