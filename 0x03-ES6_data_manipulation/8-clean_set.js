0c949c951bc030fb5c079df4640e4492cfe7e171
Elias-Dewa <elias.dewa19@gmail.com>
2023-04-25 11:18:54 -0700

    Update 8-clean_set.js

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,5 +1,5 @@
 export default function cleanSet(set, startString) {
-  if (!startString || typeof startString !== 'string' || set.size === 0) return '';
+  if (!startString || typeof startString !== 'string' || set.size === 0 || !(set instanceof Set)) return '';
 
   return [...set].filter((element) => element.startsWith(startString)).map((element) => element.slice(startString.length)).join('-');
 }
