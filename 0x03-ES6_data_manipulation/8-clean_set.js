c34e0b59232eb03ac7948880007314c547f9b3e0
Elias-Dewa <elias.dewa19@gmail.com>
2023-04-25 09:00:58 -0700

    ES6_data_manipulation: bugs are fixed and advanced task

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,5 +1,5 @@
 export default function cleanSet(set, startString) {
-  if (!startString) return "";
+  if (!startString) return '';
 
   return [...set].filter((element) => element.startsWith(startString)).map((element) => element.slice(startString.length)).join('-');
 }
