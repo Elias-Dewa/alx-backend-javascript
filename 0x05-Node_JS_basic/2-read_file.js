d82dcb83bf2a965fb13e3b0a709457b86c3fd4e7
Elias-Dewa <elias.dewa19@gmail.com>
2023-06-13 12:13:04 -0700

    Update 2-read_file.js

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,11 +1,30 @@
 const fs = require('fs');
 
 const countStudents = (path) => {
+  let fileContent;
   try {
-    let fileContent = fs.readFileSync(path, 'utf8').toString().split('\n').slice(1, fileContent.length);
-    let NUMBER_OF_STUDENTS = fileContent.length;
-    console.log(`Number of students: ${NUMBER_OF_STUDENTS}`);
+    fileContent = fs.readFileSync(path, 'utf8').toString();
   } catch (error) {
     throw new Error('Cannot load the database');
   }
-}
+  fileContent = fileContent.split('\n').filter((line) => line);
+
+  let NUMBER_OF_STUDENTS = fileContent.length;
+  console.log(`Number of students: ${NUMBER_OF_STUDENTS}`);
+
+  fileContent = fileContent.slice(1);
+  const fields = {};
+  for (const data of fileContent) {
+    const student = data.split(',');
+
+    if (!fields[student[3]]) fields[student[3]] = [];
+    fields[student[3]].push(student[0]);
+  }
+  for (const field in fields) {
+    if (field) {
+      console.log(`Number of students in ${field}: ${fields[field].length}. List: ${fields[field].join(',')}`);
+    }
+  }
+};
+
+module.exports = countStudents;
