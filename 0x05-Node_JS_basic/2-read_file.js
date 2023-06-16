cda6ad26cdd627f007f877681de8e6deaf7b6c74
Elias-Dewa <elias.dewa19@gmail.com>
2023-06-16 00:39:13 -0700

    Update 2-read_file.js

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -7,22 +7,22 @@ const countStudents = (path) => {
     fileContent = fileContent.slice(1, fileContent.length - 1);
 
     let NUMBER_OF_STUDENTS = fileContent.length;
-    console.log(`Number of students: ${NUMBER_OF_STUDENTS}`);
+    console.log(`Number of students: ${NUMBER_OF_STUDENTS}`)
+
+    const fields = {};
+    for (const data of fileContent) {
+      const student = data.split(',');
+
+      if (!fields[student[3]]) fields[student[3]] = [];
+      fields[student[3]].push(student[0]);
+    }
+    for (const field in fields) {
+      if (field) {
+        console.log(`Number of students in ${field}: ${fields[field].length}. List: ${fields[field].join(',')}`);
+      }
+    }
   } catch (error) {
     throw new Error('Cannot load the database');
-  }
-
-  const fields = {};
-  for (const data of fileContent) {
-    const student = data.split(',');
-
-    if (!fields[student[3]]) fields[student[3]] = [];
-    fields[student[3]].push(student[0]);
-  }
-  for (const field in fields) {
-    if (field) {
-      console.log(`Number of students in ${field}: ${fields[field].length}. List: ${fields[field].join(',')}`);
-    }
   }
 };
 
