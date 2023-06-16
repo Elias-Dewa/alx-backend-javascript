6c5de36888842657363b7cc359711594c9d1e6f7
Elias-Dewa <elias.dewa19@gmail.com>
2023-06-16 00:37:23 -0700

    task 2

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -3,16 +3,15 @@ const fs = require('fs');
 const countStudents = (path) => {
   let fileContent;
   try {
-    fileContent = fs.readFileSync(path, 'utf8').toString();
+    fileContent = fs.readFileSync(path, 'utf8').toString().split('\n');
+    fileContent = fileContent.slice(1, fileContent.length - 1);
+
+    let NUMBER_OF_STUDENTS = fileContent.length;
+    console.log(`Number of students: ${NUMBER_OF_STUDENTS}`);
   } catch (error) {
     throw new Error('Cannot load the database');
   }
-  fileContent = fileContent.split('\n').filter((line) => line);
 
-  let NUMBER_OF_STUDENTS = fileContent.length;
-  console.log(`Number of students: ${NUMBER_OF_STUDENTS}`);
-
-  fileContent = fileContent.slice(1);
   const fields = {};
   for (const data of fileContent) {
     const student = data.split(',');
